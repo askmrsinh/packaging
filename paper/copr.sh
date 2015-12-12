7b9d34f050420fe22c8aa40ed2b7d16e36dba0a7
Ashesh Kumar Singh <user501254@gmail.com>
2015-12-12 12:31:14 +0530

    Update Copr release script
    Changes:
    - Use version and release info similar to as available on Launchpad, making package names consistent across debian and redhat distros. This can however fail if naming format changes.
    - Check for pending launchpad imports and builds before continuing in order to make packages based on the current HEAD of project's Git repo.
    - Don't use uppercase variable names to avoid mixing them up with system variables.
    - Other fixes

--- paper/copr.sh
+++ paper/copr.sh
@@ -12,16 +12,26 @@ set -e
 set -o pipefail
 
 # Mirror Project's latest Launchpad build if it was successful and is not available in Copr
-REVISION=$(wget "https://launchpad.net/paper-icon-theme/trunk" -O - | grep -P "[0-9]{3} revisions." | grep -Po "[0-9]{3}")
-if [[ $(wget "https://copr.fedoraproject.org/api/coprs/user501254/Paper/monitor/" -O - | grep -Po '"pkg_version": "0-0.[0-9]{3}' | head -n 1| cut -c 21-) != "$REVISION" ]]; then
-  if [[ $(wget "https://launchpad.net/~snwh/+archive/ubuntu/pulp/+builds?build_text=&build_state=failed" -O - | grep "paper-icon-theme 1.0+r$REVISION~daily~ubuntu") == "" && $(wget "https://launchpad.net/~snwh/+archive/ubuntu/pulp/+builds?build_text=&build_state=built" -O - | grep "paper-icon-theme 1.0+r$REVISION~daily~ubuntu") != "" ]]; then
-    echo -e "\e[0;34mLast Launchpad build $REVISION was successful.\e[0m"
+build_info=($(wget "https://launchpad.net/~snwh/+archive/ubuntu/pulp/+builds?build_text=paper-icon-theme&build_state=all" -O - | grep -Po "build of paper-icon-theme [0-9].[0-9]\+r[0-9]{3}" | head -n 1 | grep -Po "[0-9.]{3}"))
+Version=${build_info[0]}.0
+Revision=0.${build_info[1]}
+if [[ $(wget "https://copr.fedoraproject.org/api/coprs/user501254/Paper/monitor/" -O - | grep -Po "[0-9.]{5}-[0-9.]{5}" | head -n 1) != "$Version-$Revision" ]]; then
+  if [[ $(wget "https://launchpad.net/paper-icon-theme/trunk" -O - | grep -P "[0-9]{3} revisions." | grep -Po "[0-9]{3}") != "${build_info[1]}" ]]; then
+    echo -e "\e[0;34mThese's a pending Launchpad import. Exiting.\e[0m"
+    exit
+  fi
+  if [[ $(wget "https://launchpad.net/~snwh/+archive/ubuntu/pulp/+builds?build_text=paper-icon-theme&build_state=building" -O - | grep "paper-icon-theme ${build_info[0]}+r${build_info[1]}~daily~ubuntu") != "" ]]; then
+    echo -e "\e[0;34mThese's a pending Launchpad build. Exiting.\e[0m"
+    exit
+  fi
+  if [[ $(wget "https://launchpad.net/~snwh/+archive/ubuntu/pulp/+builds?build_text=paper-icon-theme&build_state=built" -O - | grep "paper-icon-theme ${build_info[0]}+r${build_info[1]}~daily~ubuntu") != "" ]]; then
+    echo -e "\e[0;34mLast Launchpad build paper-icon-theme ${build_info[0]}+r${build_info[1]}~daily~ubuntu was a success.\e[0m"
   else
-    echo -e "\e[0;31mLast Launchpad build $REVISION failed or is being built. Exiting.\e[0m"
+    echo -e "\e[0;31mLast Launchpad build paper-icon-theme ${build_info[0]}+r${build_info[1]}~daily~ubuntu was a failure. Exiting.\e[0m"
     exit
   fi
 else
-  echo -e "\e[0;32m$REVISION is already available in Copr repo. Exiting.\e[0m"
+  echo -e "\e[0;32m\"pkg_version\" $Version-$Revision is already available in Copr repo. Exiting.\e[0m"
   exit
 fi
 
@@ -31,9 +41,9 @@ if [[ "$(git status -s --ignore-submodules)" == "" ]]; then
   git add paper-*-theme
   git commit -m "Update from origin"
   git push
-  if [[ $(wget "http://bazaar.launchpad.net/~snwh/paper-icon-theme/paper-icon-theme-git/files/$REVISION" -O - | grep "git-v1" | head -n 1 | grep -Po "[0-9a-z]{40}") == "$(git ls-tree origin/master paper-icon-theme | grep -Po [a-z0-9]{40})" && "$(git rev-parse packaging)" == "$(git rev-parse origin/packaging)" ]]; then
-    GIT_SHA=$(git ls-tree origin/master paper-icon-theme | grep -Po "[a-z0-9]{40}")
-    CHECKOUT=20151211git${GIT_SHA:0:7}
+  if [[ $(wget "http://bazaar.launchpad.net/~snwh/paper-icon-theme/paper-icon-theme-git/files/${build_info[1]}" -O - | grep "git-v1" | head -n 1 | grep -Po "[0-9a-z]{40}") == "$(git ls-tree origin/master paper-icon-theme | grep -Po [a-z0-9]{40})" && "$(git rev-parse packaging)" == "$(git rev-parse origin/packaging)" ]]; then
+    GitSha=$(git ls-tree origin/master paper-icon-theme | grep -Po "[a-z0-9]{40}")
+    Checkout=20151212git${GitSha:0:7}
   else
     git status
     echo -e "\e[0;31m\nExiting.\e[0m"
@@ -49,25 +59,25 @@ fi
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 
 # Generate tarball, similar in structure to those that are downloaded from GitHub
-tar -czf "rpmbuild/SOURCES/paper-icon-theme-$GIT_SHA.tar.gz" paper-icon-theme/* --transform s/paper-icon-theme/paper-icon-theme-$GIT_SHA/
+tar -czf "rpmbuild/SOURCES/paper-icon-theme-$GitSha.tar.gz" paper-icon-theme/* --transform "s/paper-icon-theme/paper-icon-theme-$GitSha/"
 
 # Set commit0 value within spec file
-sed -i "s/%global commit0 .*/$GIT_SHA/" rpmbuild/SPECS/paper-icon-theme.spec
+sed -i "s/%global commit0 .*/%global commit0 $GitSha/" rpmbuild/SPECS/paper-icon-theme.spec
 
 # Set version and release tag value within spec file
-sed -i "s/version:    .*/version:    0/; s/release:    .*/release:    0.$REVISION.$CHECKOUT%{?dist}/" rpmbuild/SPECS/paper-icon-theme.spec
+sed -i "s/version:    .*/version:    $Version/; s/release:    .*/release:    $Revision.$Checkout%{?dist}/" rpmbuild/SPECS/paper-icon-theme.spec
 
 # Build only Source RPM
 rpmbuild --define "_topdir $PWD/rpmbuild" -bs rpmbuild/SPECS/paper-icon-theme.spec
 
 # Submit Source RPM to Copr
-PKG_VERSION=0-0.$REVISION.$CHECKOUT.$(uname -a | grep -Po "fc[0-9]{2}")
-echo -e "\e[0;34m\nUploading paper-icon-theme-$PKG_VERSION.src.rpm to Copr\e[0m"
-copr-cli build Paper "rpmbuild/SRPMS/paper-icon-theme-$PKG_VERSION.src.rpm" -r epel-7-x86_64 -r fedora-22-i386 -r fedora-22-x86_64 -r fedora-23-i386 -r fedora-23-x86_64
+PkgVersion=$Version-$Revision.$Checkout.$(uname -a | grep -Po "fc[0-9]{2}")
+echo -e "\e[0;34m\nUploading paper-icon-theme-$PkgVersion.src.rpm to Copr\e[0m"
+copr-cli build Paper "rpmbuild/SRPMS/paper-icon-theme-$PkgVersion.src.rpm" -r epel-7-x86_64 -r fedora-22-i386 -r fedora-22-x86_64 -r fedora-23-i386 -r fedora-23-x86_64
 
 # Cleanup
-rm -r rpmbuild/SRPMS/paper-icon-theme-$PKG_VERSION.src.rpm
-rm -r rpmbuild/SOURCES/paper-icon-theme-$GIT_SHA.tar.gz
+rm -r "rpmbuild/SRPMS/paper-icon-theme-$PkgVersion.src.rpm"
+rm -r "rpmbuild/SOURCES/paper-icon-theme-$GitSha.tar.gz"
 
 # Update
 git add rpmbuild/SPECS/paper-icon-theme.spec
